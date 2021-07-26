9af47e8b5a7eaa2d3f316c68fb6cad71777174ec
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-07-26 14:45:24 +0530

    Inspirational quotes

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict"
+const quotation = document.querySelector(".quotation");
+const newQuoteBtn = document.querySelector(".btn-new-quote");
+const quoteContainer = document.querySelector(".quote-container");
+
+const showButton = () => {
+    newQuoteBtn.style.display = "block";
+}
+
+const displayQuote = () => {
+
+    //fetch(`https://game-of-thrones-quotes.herokuapp.com/v1/random/5`)
+      fetch(`https://secret-ocean-49799.herokuapp.com/https://inspiration.goprogram.ai/`)
+        .then(response => {
+            console.log(response);
+            if(!response.ok) {
+                throw new Error(`No quote to display.`)
+            }
+            return response.json()
+        })
+        .then(data => {
+            const newData = ({...data});
+           quotation.textContent = newData.quote;
+            quoteContainer.classList.remove("hidden");
+        })
+        .catch(err => console.log(`${err}`))
+}
+displayQuote();
+
+newQuoteBtn.addEventListener("click", displayQuote);
